d6c97d8f73b508fe5faa2d1638a34c0235ab3b05
Artsiom Safonau <ArtemSafonau@ibagroup.eu>
2023-09-07 14:25:51 +0300

    Change link route param

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :links, except: %i[edit update]
+  resources :links, except: %i[edit update], param: :short_url
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
