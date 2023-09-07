1128a6782d4bb257b9f1a4ba42d8f931fb2fb96e
Artsiom Safonau <ArtemSafonau@ibagroup.eu>
2023-09-07 14:29:01 +0300

    Add default value for clicked field

--- db/migrate/20230907111256_create_links.rb
+++ db/migrate/20230907111256_create_links.rb
@@ -5,7 +5,7 @@ class CreateLinks < ActiveRecord::Migration[7.0]
     create_table :links do |t|
       t.string :original_url
       t.string :short_url
-      t.integer :clicked
+      t.integer :clicked, default: 0
     end
     add_index :links, :short_url, unique: true
   end
