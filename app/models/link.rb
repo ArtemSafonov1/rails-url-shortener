f35f50b2f1c57e5502ff26b5854ef7f4253d05ce
Artsiom Safonau <ArtemSafonau@ibagroup.eu>
2023-09-08 11:54:19 +0300

    Update model, add validations

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Link < ApplicationRecord
+  validates :original_url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(['http','https'])
+  validates :short_url, uniqueness: true
+
+  before_validation :generate_short_url
+
+  def generate_short_url
+    self.short_url = SecureRandom.uuid[0..7]
+  end
+
+  def click
+    self.increment!(:clicked)
+  end
 end
