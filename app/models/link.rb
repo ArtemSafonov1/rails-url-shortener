17701ee61cf7297483e5afd555e0b0ef3a4226ba
Artsiom Safonau <ArtemSafonau@ibagroup.eu>
2023-09-08 13:55:25 +0300

    Update genarate method

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,13 +4,15 @@ class Link < ApplicationRecord
   validates :original_url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
   validates :short_url, uniqueness: true
 
-  before_validation :generate_short_url
-
-  def generate_short_url
-    self.short_url = "/s/#{SecureRandom.uuid[0..7]}"
-  end
+  before_validation :generate_short_url_slug
 
   def click
     increment!(:clicked)
+  end
+
+  private
+
+  def generate_short_url_slug
+    self.short_url = SecureRandom.uuid[0..7]
   end
 end
